6809ea9f8673dc6ad2c31d4749c4b31d857aca7b
Luka Tarman <luka.tarman@gmail.com>
2022-11-24 12:33:58 +0100

    added bucket table component

---
+++ src/components/BucketTable/bucket.table.view.js
@@ -0,0 +1,15 @@
+const BucketTable = () => {
+  return (
+    <div>
+      <div>
+        <div>all buckets</div>
+        <button type="button" value="">
+          Create Bucket
+        </button>
+      </div>
+      <div>table</div>
+    </div>
+  );
+};
+
+export default BucketTable;
