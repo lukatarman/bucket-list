b23a59d8961e1169768bf0d6b385f874ac888602
Luka Tarman <luka.tarman@gmail.com>
2022-11-24 12:34:04 +0100

    added create bucket component

---
+++ src/components/CreateBucket/create.bucket.view.js
@@ -0,0 +1,13 @@
+const CreateBucket = () => {
+  return (
+    <div>
+      <h5>Create new bucket</h5>
+      <div>
+        <div>Bucket inputs</div>
+        <div>Create bucket button</div>
+      </div>
+    </div>
+  );
+};
+
+export default CreateBucket;
