a75ab903e5691c0b112519fe657498ba703f4121
Luka Tarman <luka.tarman@gmail.com>
2022-11-24 16:04:57 +0100

    instantiated classes, added imports

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,1 +1,9 @@
-console.log("my test");
+import { Database } from "./infrastructure/database.client.js";
+import { QueriesRouter } from "./router.js";
+import { WebServer } from "./infrastructure/web.server.js";
+
+const database = new Database();
+const queriesRouter = new QueriesRouter(database);
+const webServer = new WebServer(queriesRouter);
+
+webServer.start();
