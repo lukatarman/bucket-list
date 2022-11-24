531fd1969f3699a5bf71a54952ca739ddc2e1675
Luka Tarman <luka.tarman@gmail.com>
2022-11-24 17:55:45 +0100

    added proper API request to client adapter

--- src/adapters/http-client-adapter.js
+++ src/adapters/http-client-adapter.js
@@ -1,17 +1,5 @@
-export async function getData() {
-  const response = await fetch("http://localhost:3000/database-response.json");
-  return await response.json();
-}
+import axios from "axios";
 
-export async function postData(data) {
-  fetch("http://localhost:3000/database-response.json", {
-    method: "POST",
-    headers: {
-      "Content-type": "application/json",
-    },
-    body: JSON.stringify({
-      name: data.name,
-      location: data.location,
-    }),
-  });
+export async function getData() {
+  return await axios.get("http://localhost:3000/bucketList");
 }
