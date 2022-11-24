6be2da6a818b2c48ff077ab256828eb39e814b2c
Luka Tarman <luka.tarman@gmail.com>
2022-11-24 11:27:29 +0100

    added BrowserRouter component

--- src/index.js
+++ src/index.js
@@ -3,4 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App/app.view.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
