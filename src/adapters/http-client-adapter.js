61b3f34225009162fb18b34ec0562239352a8663
Luka Tarman <luka.tarman@gmail.com>
2022-11-24 15:23:10 +0100

    set up backend folder
    # installed fastify

--- src/adapters/http-client-adapter.js
+++ src/adapters/http-client-adapter.js
@@ -2,3 +2,16 @@ export async function getData() {
   const response = await fetch("http://localhost:3000/database-response.json");
   return await response.json();
 }
+
+export async function postData(data) {
+  fetch("http://localhost:3000/database-response.json", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({
+      name: data.name,
+      location: data.location,
+    }),
+  });
+}
