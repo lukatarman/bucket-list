ad114549c572373c63933bb2723fd19c2ccd8525
Luka Tarman <luka.tarman@gmail.com>
2022-11-24 13:18:12 +0100

    passed down state into components

--- src/Pages/BucketList/bucket.list.view.js
+++ src/Pages/BucketList/bucket.list.view.js
@@ -1,11 +1,24 @@
+import { useState } from "react";
 import BucketTable from "../../components/BucketTable/bucket.table.view.js";
 import CreateBucket from "../../components/CreateBucket/create.bucket.view.js";
 
 const BucketList = () => {
+  const [displayCreateBucket, setDisplayCreateBucket] = useState(false);
+  const [displayCreateButton, setDisplayCreateButton] = useState(true);
+
   return (
     <div>
-      <CreateBucket />
-      <BucketTable />
+      {displayCreateBucket ? (
+        <CreateBucket
+          setDisplayCreateBucket={setDisplayCreateBucket}
+          setDisplayCreateButton={setDisplayCreateButton}
+        />
+      ) : null}
+      <BucketTable
+        setDisplayCreateBucket={setDisplayCreateBucket}
+        setDisplayCreateButton={setDisplayCreateButton}
+        displayCreateButton={displayCreateButton}
+      />
     </div>
   );
 };
