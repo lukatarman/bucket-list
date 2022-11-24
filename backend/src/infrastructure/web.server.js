675fa13f8c58d7d57b9693115fc902480113895c
Luka Tarman <luka.tarman@gmail.com>
2022-11-24 16:05:11 +0100

    added web server

---
+++ backend/src/infrastructure/web.server.js
@@ -0,0 +1,32 @@
+import Fastify from "fastify";
+
+export class WebServer {
+  #server;
+
+  constructor(gameQueriesRouter) {
+    this.#server = Fastify({
+      logger: true,
+    });
+
+    this.#server.register(gameQueriesRouter.routes);
+  }
+
+  async start() {
+    this.#server.log.info("Starting server...");
+    try {
+      await this.#server.listen(3000);
+    } catch (err) {
+      this.#server.log.error(err);
+      process.exit(1);
+    }
+  }
+
+  async stop() {
+    this.#server.log.info("Stopping server...");
+    try {
+      this.#server.close();
+    } catch (err) {
+      this.#server.log.error(err);
+    }
+  }
+}
