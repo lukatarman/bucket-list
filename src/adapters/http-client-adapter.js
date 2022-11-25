9ce20cc83e145431891c81770dfc5568518f5990
Luka Tarman <luka.tarman@gmail.com>
2022-11-25 10:39:00 +0100

    added postData function to adapter

--- src/adapters/http-client-adapter.js
+++ src/adapters/http-client-adapter.js
@@ -3,3 +3,6 @@ import axios from "axios";
 export async function getData() {
   return await axios.get("http://localhost:3000/bucketList");
 }
+export function postData({ name, location }) {
+  axios.post("http://localhost:3000/buckets/add", { name, location });
+}
