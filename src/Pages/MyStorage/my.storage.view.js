fdeddc33d1d508be6d6209031a8ee8cd7797c461
Luka Tarman <luka.tarman@gmail.com>
2022-11-25 22:03:25 +0100

    added MyStorage component and JSX

--- src/Pages/MyStorage/my.storage.view.js
+++ src/Pages/MyStorage/my.storage.view.js
@@ -1,12 +1,33 @@
+import { useState } from "react";
+import FileList from "../../components/FileList/file.list.view.js";
+import FileDetails from "../../components/FileDetails/file.details.view.js";
+
 const MyStorage = () => {
+  const [visibleTab, setVisibleTab] = useState("file-list");
+
+  const displayTab = () => {
+    if (visibleTab === "file-list") return <FileList />;
+    if (visibleTab === "file-details") return <FileDetails />;
+  };
   return (
     <div>
       <h1>MyNewStorage</h1>
       <div>
-        <button>Files</button>
-        <button>Details</button>
-        {/* <FileList />
-        <FileDetails /> */}
+        <button
+          onClick={() => {
+            setVisibleTab("file-list");
+          }}
+        >
+          Files
+        </button>
+        <button
+          onClick={() => {
+            setVisibleTab("file-details");
+          }}
+        >
+          Details
+        </button>
+        {displayTab()}
       </div>
     </div>
   );
