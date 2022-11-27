f065b8cd51950ae72ad3b3ff9cf592c35b4deac4
Luka Tarman <luka.tarman@gmail.com>
2022-11-27 14:05:27 +0100

    added JSX to component

--- src/components/FileDetails/file.details.view.js
+++ src/components/FileDetails/file.details.view.js
@@ -1,5 +1,12 @@
-const FileDetails = () => {
-  return <div>File Details</div>;
+const FileDetails = ({ selectedBucket }) => {
+  console.log(selectedBucket);
+  return (
+    <div>
+      <div>{selectedBucket.name}</div>
+      <div>{selectedBucket.location}</div>
+      <div>{selectedBucket.size}</div>
+    </div>
+  );
 };
 
 export default FileDetails;
