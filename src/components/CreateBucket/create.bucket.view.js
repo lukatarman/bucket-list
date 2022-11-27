4454581c7d7e4921e25518e80ec4268219415bd3
Luka Tarman <luka.tarman@gmail.com>
2022-11-27 17:59:21 +0100

    added styling

--- src/components/CreateBucket/create.bucket.view.js
+++ src/components/CreateBucket/create.bucket.view.js
@@ -1,3 +1,4 @@
+import { Row, Col, Form, Dropdown, Button, ButtonGroup } from "react-bootstrap";
 import { getBucketList } from "../../adapters/http.client.adapter.js";
 import { postData } from "../../adapters/http.client.adapter.js";
 import { useState } from "react";
@@ -30,28 +31,41 @@ const CreateBucket = ({
 
   return (
     <div>
-      <h5>Create new bucket</h5>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <div>
-            <label>Bucket Name*</label>
-            <input
+      <div className="px-3">Create new bucket</div>
+      <form className="bg-white p-3 mb-3" onSubmit={handleSubmit}>
+        <Row>
+          <Col>
+            <Form.Label htmlFor="name">Bucket Name*</Form.Label>
+            <Form.Control
               type="text"
               name="name"
+              id="name"
               value={nameInputValue}
               onChange={onNameChange}
-            />
-          </div>
-          <div>
-            <label>Bucket Location*</label>
-            <input
-              type="text"
-              name="location"
-              value={locationvalue}
-              onChange={onLocationChange}
-            />
-          </div>
-        </div>
+            ></Form.Control>
+          </Col>
+          <Col>
+            <Form.Label htmlFor="name">Bucket Location*</Form.Label>
+            <Dropdown as={ButtonGroup}>
+              <Button className="py-0" variant="custom-dropdown">
+                Split Button
+              </Button>
+
+              <Dropdown.Toggle
+                className="py-0"
+                split
+                variant="custom-dropdown-toggler"
+                id="dropdown-split-basic"
+              />
+
+              <Dropdown.Menu>
+                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </Col>
+        </Row>
 
         <button type="submit">Create Bucket</button>
       </form>
