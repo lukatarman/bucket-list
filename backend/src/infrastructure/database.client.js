9c4d129e513f7097553373787753f3973bb904a5
Luka Tarman <luka.tarman@gmail.com>
2022-11-27 11:05:47 +0100

    added method

--- backend/src/infrastructure/database.client.js
+++ backend/src/infrastructure/database.client.js
@@ -4,6 +4,17 @@ export class Database {
     return fs.readFileSync("./src/assets/database-response.json", "utf-8");
   }
 
+  getFileList(bucketName) {
+    const bucketContent = JSON.parse(
+      fs.readFileSync("./src/assets/database-response.json", "utf-8")
+    );
+
+    const bucketIndex = bucketContent.findIndex((bucket) => bucket.name === bucketName);
+
+    console.log(bucketContent[bucketIndex]);
+    return bucketContent[bucketIndex].files;
+  }
+
   addBucketListEntry(data) {
     const bucketContent = JSON.parse(
       fs.readFileSync("./src/assets/database-response.json", "utf-8")
