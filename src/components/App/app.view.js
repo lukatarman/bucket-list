fbe1e665182a4d461b3038ad909788210e8ef57d
Luka Tarman <luka.tarman@gmail.com>
2022-11-27 11:08:02 +0100

    passed state into components
    # added proper JSX

--- src/components/App/app.view.js
+++ src/components/App/app.view.js
@@ -1,13 +1,34 @@
 import BucketList from "../../pages/BucketList/bucket.list.view.js";
 import MyStorage from "../../pages/MyStorage/my.storage.view.js";
+import { useState } from "react";
 import "../../assets/global.css";
 
 const App = () => {
+  const [selectedBucket, setSelectedBucket] = useState("");
+  const [visiblePage, setVisiblePage] = useState("bucket-list");
+
+  const render = () => {
+    if (visiblePage === "bucket-list")
+      return (
+        <BucketList
+          setSelectedBucket={setSelectedBucket}
+          setVisiblePage={setVisiblePage}
+        />
+      );
+    if (visiblePage === "my-storage")
+      return <MyStorage selectedBucket={selectedBucket} />;
+  };
+
   return (
     <div>
-      <nav>Secure could storage</nav>
-      <BucketList />
-      <MyStorage />
+      <nav
+        onClick={() => {
+          setVisiblePage("bucket-list");
+        }}
+      >
+        Secure could storage
+      </nav>
+      {render()}
     </div>
   );
 };
