98dc71557f8c2e94ef649033122149010e710af9
Luka Tarman <luka.tarman@gmail.com>
2022-11-27 11:06:37 +0100

    added API function

--- src/adapters/http.client.adapter.js
+++ src/adapters/http.client.adapter.js
@@ -4,6 +4,10 @@ export async function getBucketList() {
   return await axios.get("http://localhost:3000/bucketList");
 }
 
+export async function getFileList(selectedBucket) {
+  return await axios.post("http://localhost:3000/fileList", { selectedBucket });
+}
+
 export async function postData({ name, location }) {
   axios.post("http://localhost:3000/buckets/add/listEntry", { name, location });
 }
