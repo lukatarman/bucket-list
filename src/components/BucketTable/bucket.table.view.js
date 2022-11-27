1bd23ec60a55a20f774143f0a94f4192e1a4ddc5
Luka Tarman <luka.tarman@gmail.com>
2022-11-27 17:58:55 +0100

    added bootstrap styling to JSX

--- src/components/BucketTable/bucket.table.view.js
+++ src/components/BucketTable/bucket.table.view.js
@@ -1,3 +1,4 @@
+import { Col, Row, Button, Table } from "react-bootstrap";
 import { useEffect } from "react";
 import { getBucketList } from "../../adapters/http.client.adapter.js";
 
@@ -26,38 +27,55 @@ const BucketTable = ({
 
   const showButton = () => {
     return (
-      <button type="button" value="" onClick={onButtonClick}>
-        Create Bucket
-      </button>
+      <Button className="py-0" variant="custom" type="button" onClick={onButtonClick}>
+        Create New Bucket
+      </Button>
     );
   };
 
   const tableRender = tableResults.map((result, index) => {
     return (
-      <div
-        key={index}
-        onClick={() => {
-          setSelectedBucket({
-            name: result.name,
-            location: result.location,
-            size: result.size,
-            index,
-          });
-          setVisiblePage("my-storage");
-        }}
-      >
-        {result.name}:{result.location}
-      </div>
+      <tbody>
+        <tr
+          key={index}
+          onClick={() => {
+            setSelectedBucket({
+              name: result.name,
+              location: result.location,
+              size: result.size,
+              index,
+            });
+            setVisiblePage("my-storage");
+          }}
+        >
+          <td>{result.name}</td>
+          <td>{result.location}</td>
+        </tr>
+      </tbody>
     );
   });
 
   return (
-    <div>
-      <div>
-        <div>all buckets ({tableResults.length})</div>
-        <div>{displayCreateButton ? showButton() : null}</div>
-      </div>
-      <div>{tableRender}</div>
+    <div className="p-3 bg-white">
+      <Row className="mb-3">
+        <Col className="d-flex align-items-center">
+          <div>All Buckets ({tableResults.length})</div>
+        </Col>
+        <Col className="d-flex justify-content-end">
+          {displayCreateButton ? showButton() : null}
+        </Col>
+      </Row>
+      <Table borderless hover>
+        <thead>
+          <tr className="table-row-bordered">
+            <th width="70%" className="table-head-content">
+              Name
+            </th>
+            <th className="table-head-content">Location</th>
+          </tr>
+        </thead>
+        {tableRender}
+      </Table>
     </div>
   );
 };
