a38b14bf735848e8d6f0f45ca7ed48839a20cbf8
Luka Tarman <luka.tarman@gmail.com>
2022-11-28 06:57:26 +0100

    fixed myStorageView component

--- src/Pages/MyStorage/my.storage.view.js
+++ src/Pages/MyStorage/my.storage.view.js
@@ -1,5 +1,5 @@
-import { Container, Tab, Tabs, Sonnet } from "react-bootstrap";
-import { useState } from "react";
+import { Container, Tab, Tabs } from "react-bootstrap";
+import { useState, useEffect } from "react";
 import FileList from "../../components/FileList/file.list.view.js";
 import FileDetails from "../../components/FileDetails/file.details.view.js";
 import { deleteBucket } from "../../adapters/http.client.adapter.js";
@@ -9,33 +9,32 @@ const MyStorage = ({ selectedBucket, setVisiblePage }) => {
   const [filesTable, setFilesTable] = useState([]);
   const [visibleDelete, setVisibleDelete] = useState(false);
 
-  const displayTab = () => {
-    if (visibleTab === "file-list")
-      return (
-        <FileList
-          selectedBucket={selectedBucket}
-          filesTable={filesTable}
-          setFilesTable={setFilesTable}
-        />
-      );
-    if (visibleTab === "file-details")
-      return <FileDetails filesTable={filesTable} selectedBucket={selectedBucket} />;
-  };
+  useEffect(() => {
+    console.log("visible tab component changed");
+    setVisibleDelete(!visibleDelete);
+    console.log(`swapped visible delete to ${visibleDelete}`);
+  }, [visibleTab]);
 
   const handleDeleteClick = () => {
     deleteBucket(selectedBucket.index);
     setVisiblePage("bucket-list");
   };
 
+  const handleTabSelect = (key) => {
+    setVisibleTab(key);
+    console.log(`visible tab changed to ${key}`);
+  };
+
   return (
     <Container fluid className="px-5">
       <h4 className="my-3">{selectedBucket.name}</h4>
-      <div>
+      <div className="custom-position">
+        <div>TEST</div>
         <Tabs
           className="my-test"
           id="controlled-tab-example"
           activeKey={visibleTab}
-          onSelect={(key) => setVisibleTab(key)}
+          onSelect={handleTabSelect}
         >
           <Tab eventKey="file-list" title="Files" className="my-test-now">
             <FileList
@@ -48,6 +47,15 @@ const MyStorage = ({ selectedBucket, setVisiblePage }) => {
             <FileDetails filesTable={filesTable} selectedBucket={selectedBucket} />
           </Tab>
         </Tabs>
+        <div className="custom-button-position">
+          <div>
+            {!visibleDelete ? (
+              <button className="custom-delete-button" onClick={handleDeleteClick}>
+                Delete Bucket
+              </button>
+            ) : null}
+          </div>
+        </div>
       </div>
     </Container>
   );
