7b53eac2fa266a1a587ad991f09840b85bf1e89f
Luka Tarman <luka.tarman@gmail.com>
2022-11-28 10:55:36 +0100

    added styling

--- src/components/FileDetails/file.details.view.js
+++ src/components/FileDetails/file.details.view.js
@@ -1,9 +1,18 @@
+import AlertPopup from "../AlertPopup/AlertPopup.js";
+
 const FileDetails = ({ selectedBucket }) => {
   return (
-    <div>
-      <div>{selectedBucket.name}</div>
-      <div>{selectedBucket.location}</div>
-      <div>{selectedBucket.size}</div>
+    <div className="px-5 bg-white py-3 d-flex flex-row">
+      <div className="p-3">
+        <div>Bucket name:</div>
+        <div>Location:</div>
+        <div>Storage size:</div>
+      </div>
+      <div className="p-3">
+        <div>{selectedBucket.name}</div>
+        <div>{selectedBucket.location}</div>
+        <div>{selectedBucket.size}</div>
+      </div>
     </div>
   );
 };
