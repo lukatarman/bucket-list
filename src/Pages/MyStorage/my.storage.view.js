5a729a39ceb79db697681a2fb2d86e96a7d4d54e
Luka Tarman <luka.tarman@gmail.com>
2022-11-28 10:56:23 +0100

    used alert popup component

--- src/Pages/MyStorage/my.storage.view.js
+++ src/Pages/MyStorage/my.storage.view.js
@@ -2,12 +2,14 @@ import { Container, Tab, Tabs } from "react-bootstrap";
 import { useState, useEffect } from "react";
 import FileList from "../../components/FileList/file.list.view.js";
 import FileDetails from "../../components/FileDetails/file.details.view.js";
+import AlertPopup from "../../components/AlertPopup/AlertPopup.js";
 import { deleteBucket } from "../../adapters/http.client.adapter.js";
 
 const MyStorage = ({ selectedBucket, setVisiblePage }) => {
   const [visibleTab, setVisibleTab] = useState("file-list");
   const [filesTable, setFilesTable] = useState([]);
   const [visibleDelete, setVisibleDelete] = useState(false);
+  const [showDetailsAlert, setShowDetailsAlert] = useState(false);
 
   useEffect(() => {
     console.log("visible tab component changed");
@@ -27,30 +29,42 @@ const MyStorage = ({ selectedBucket, setVisiblePage }) => {
 
   return (
     <Container fluid className="px-5">
+      <AlertPopup
+        showAlert={showDetailsAlert}
+        setShowAlert={setShowDetailsAlert}
+        handleDelete={handleDeleteClick}
+      />
       <h4 className="my-3">{selectedBucket.name}</h4>
       <div className="custom-position">
-        <div>TEST</div>
         <Tabs
-          className="my-test"
+          className="custom-test"
           id="controlled-tab-example"
           activeKey={visibleTab}
           onSelect={handleTabSelect}
         >
-          <Tab eventKey="file-list" title="Files" className="my-test-now">
+          <Tab eventKey="file-list" title="Files">
             <FileList
               selectedBucket={selectedBucket}
               filesTable={filesTable}
               setFilesTable={setFilesTable}
             />
           </Tab>
-          <Tab eventKey="file-details" title="Details" className="bg-white pb-3">
-            <FileDetails filesTable={filesTable} selectedBucket={selectedBucket} />
+          <Tab eventKey="file-details" title="Details">
+            <FileDetails
+              filesTable={filesTable}
+              selectedBucket={selectedBucket}
+              showDetailsAlert={showDetailsAlert}
+              setShowDetailsAlert={setShowDetailsAlert}
+            />
           </Tab>
         </Tabs>
-        <div className="custom-button-position">
+        <div className="custom-button-position p-1">
           <div>
             {!visibleDelete ? (
-              <button className="custom-delete-button" onClick={handleDeleteClick}>
+              <button
+                className="custom-delete-button px-2"
+                onClick={() => setShowDetailsAlert(true)}
+              >
                 Delete Bucket
               </button>
             ) : null}
