454dcc984c53417ced9bfd3692f0f9c9b5661494
Luka Tarman <luka.tarman@gmail.com>
2022-11-30 11:51:22 +0100

    refactored addFile method

--- backend/src/infrastructure/storage.client.js
+++ backend/src/infrastructure/storage.client.js
@@ -32,36 +32,13 @@ export class StorageClient {
   }
 
   async addFile(fileDetails, bucketIndex) {
-    console.log(fileDetails);
-    const bucketContent = JSON.parse(
-      fs.readFileSync("./src/assets/database-response.json", "utf-8")
-    );
+    const existingData = this.#getParsedFile();
 
-    bucketContent[bucketIndex].files.push({
-      name: fileDetails.originalname,
-      lastModified: this.fixedDate,
-      size: fixBytes(fileDetails.size),
-      buffer: fileDetails.buffer,
-    });
+    fileDetails.lastModified = fixDate();
 
-    fs.writeFileSync(
-      "./src/assets/database-response.json",
-      JSON.stringify(bucketContent),
-      (err) => {
-        if (err) {
-          console.error(err);
-          return;
-        }
-        console.log("file has been updated");
-      }
-    );
-  }
+    existingData[bucketIndex].files.push(new File(fileDetails));
 
-  get fixedDate() {
-    const currentDate = new Date();
-    const fixedDay = ("0" + currentDate.getDate()).slice(-2);
-    const fixedMonth = ("0" + (currentDate.getMonth() + 1)).slice(-2);
-    return `${fixedDay}.${fixedMonth}.${currentDate.getFullYear()}`;
+    this.#saveFileWithNewData(existingData);
   }
 
   deleteFile(bucketIndex) {
