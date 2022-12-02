af3292e71a97527ba1cacd920a43d07331249373
Luka Tarman <luka.tarman@gmail.com>
2022-12-02 15:21:04 +0100

    added content to AppBehavior

--- src/Pages/App/app.behavior.js
+++ src/Pages/App/app.behavior.js
@@ -1,3 +1,23 @@
-const AppBehavoir = () => {};
+const AppBehavoir = () => {
+  const handleBrandClick = () => {
+    setVisiblePage("bucket-list");
+  };
+
+  const render = () => {
+    if (visiblePage === "bucket-list")
+      return (
+        <BucketList
+          setSelectedBucket={setSelectedBucket}
+          setVisiblePage={setVisiblePage}
+        />
+      );
+    if (visiblePage === "my-storage")
+      return (
+        <MyStorage selectedBucket={selectedBucket} setVisiblePage={setVisiblePage} />
+      );
+  };
+
+  return [handleBrandClick, render];
+};
 
 export default AppBehavoir;
