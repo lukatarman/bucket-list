b43508dec59f246d6486d52a8c8fcf9865b0c1ed
Luka Tarman <luka.tarman@gmail.com>
2022-12-02 15:14:34 +0100

    added new location for app component

---
+++ src/Pages/App/app.view.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+import BucketList from "../BucketList/bucket.list.view.js";
+import MyStorage from "../MyStorage/my.storage.view.js";
+import { Navbar } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import { useState } from "react";
+import "../../assets/customStyles.css";
+import { useRecoilValue } from "recoil";
+import { todoListState } from "../../components/FileList/file.list.view.js";
+
+const App = () => {
+  const [selectedBucket, setSelectedBucket] = useState("");
+  const [visiblePage, setVisiblePage] = useState("bucket-list");
+
+  const handleBrandClick = () => {
+    setVisiblePage("bucket-list");
+  };
+
+  const render = () => {
+    if (visiblePage === "bucket-list")
+      return (
+        <BucketList
+          setSelectedBucket={setSelectedBucket}
+          setVisiblePage={setVisiblePage}
+        />
+      );
+    if (visiblePage === "my-storage")
+      return (
+        <MyStorage selectedBucket={selectedBucket} setVisiblePage={setVisiblePage} />
+      );
+  };
+
+  return (
+    <div className="app-background">
+      <Navbar variant="custom" className="px-3">
+        <Container fluid>
+          <h4 className="brand-custom" onClick={handleBrandClick}>
+            Secure cloud storage
+          </h4>
+        </Container>
+      </Navbar>
+      {render()}
+    </div>
+  );
+};
+
+export default App;
