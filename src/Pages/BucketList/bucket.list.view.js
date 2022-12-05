5c2fea75ff5da060a7377ec5ec6a6ac1741f4ec3
Luka Tarman <luka.tarman@gmail.com>
2022-12-05 13:13:33 +0100

    added neccesary import

--- src/Pages/BucketList/bucket.list.view.js
+++ src/Pages/BucketList/bucket.list.view.js
@@ -1,6 +1,7 @@
 import { Container } from "react-bootstrap";
 import BucketTable from "../../components/BucketTable/bucket.table.view.js";
 import CreateBucket from "../../components/CreateBucket/create.bucket.view.js";
+import { useRecoilValue } from "recoil";
 import { displayCreateBucketState } from "../../contexts/BucketListContext/index.js";
 
 const BucketList = () => {
