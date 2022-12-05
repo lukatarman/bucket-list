a1f5bdabad81abb55b2d78914bbfb0527bacc244
Luka Tarman <luka.tarman@gmail.com>
2022-12-05 13:06:11 +0100

    used new state

--- src/Pages/BucketList/bucket.list.view.js
+++ src/Pages/BucketList/bucket.list.view.js
@@ -1,10 +1,11 @@
 import { Container } from "react-bootstrap";
-import { useState } from "react";
 import BucketTable from "../../components/BucketTable/bucket.table.view.js";
 import CreateBucket from "../../components/CreateBucket/create.bucket.view.js";
 import { displayCreateBucketState } from "../../contexts/BucketListContext/index.js";
 
 const BucketList = ({ setSelectedBucket, setVisiblePage }) => {
+  const displayCreateBucket = useRecoilValue(displayCreateBucketState);
+
   return (
     <Container fluid className="px-5">
       <h4 className="my-3">Bucket List</h4>
