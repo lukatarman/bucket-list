51ae16a6434bcbe581d751a8539cb2c7c6f8b907
Luka Tarman <luka.tarman@gmail.com>
2022-12-06 13:12:20 +0100

    added TableItem component

---
+++ src/components/TableItem/table.item.js
@@ -0,0 +1,12 @@
+const TableItem = ({ item, handleTableItemClick }) => {
+  return (
+    <tbody>
+      <tr className="table-row-content" onClick={() => handleTableItemClick(item)}>
+        <td>{item.name}</td>
+        <td>{item.location}</td>
+      </tr>
+    </tbody>
+  );
+};
+
+export default TableItem;
