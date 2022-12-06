a10920a3759d85cae1769df44fb45ab5b5b86c39
Luka Tarman <luka.tarman@gmail.com>
2022-12-06 13:12:09 +0100

    extracted behavior in separate file

--- src/components/BucketTable/bucket.table.view.js
+++ src/components/BucketTable/bucket.table.view.js
@@ -1,49 +1,13 @@
 import { Table } from "react-bootstrap";
-import { useEffect } from "react";
-import { getBuckets } from "../../adapters/http.client.adapter.js";
-import { useRecoilState, useSetRecoilState } from "recoil";
-import { bucketListTableResultsState } from "../../contexts/BucketListContext/index.js";
-import {
-  selectedBucketState,
-  visiblePageState,
-} from "../../contexts/AppContext/index.js";
+import BucketTableBehavior from "./bucket.table.behavior.js";
+import TableItem from "../TableItem/table.item.js";
 
 const BucketTable = () => {
-  const [tableResults, setTableResults] = useRecoilState(bucketListTableResultsState);
-
-  const setSelectedBucket = useSetRecoilState(selectedBucketState);
-  const setVisiblePage = useSetRecoilState(visiblePageState);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await getBuckets();
-      setTableResults(response.data);
-    };
-
-    fetchData();
-  }, [setTableResults]);
-
-  const handleTableItemClick = (result, index) => {
-    setSelectedBucket({
-      name: result.name,
-      location: result.location,
-      size: result.size,
-      index,
-    });
-    setVisiblePage("my-storage");
-  };
+  const [tableResults, handleTableItemClick] = BucketTableBehavior();
 
   const tableRender = tableResults.map((result, index) => {
     return (
-      <tbody key={index}>
-        <tr
-          className="table-row-content"
-          onClick={() => handleTableItemClick(result, index)}
-        >
-          <td>{result.name}</td>
-          <td>{result.location}</td>
-        </tr>
-      </tbody>
+      <TableItem key={index} item={result} handleTableItemClick={handleTableItemClick} />
     );
   });
 
