b7b0f601623d99d72ec9728a96cba5c7aab3852e
Luka Tarman <luka.tarman@gmail.com>
2022-12-06 10:57:57 +0100

    refactored code

--- src/components/CreateBucket/create.bucket.behavior.js
+++ src/components/CreateBucket/create.bucket.behavior.js
@@ -1,8 +1,17 @@
-const CreateBucketBehavior = (
-  setDisplayCreateBucket,
-  setDisplayCreateButton,
-  setTableResults
-) => {
+import { useState } from "react";
+import { useSetRecoilState } from "recoil";
+import {
+  displayCreateBucketState,
+  displayCreateButtonState,
+  bucketListTableResultsState,
+} from "../../contexts/BucketListContext/index.js";
+import { createBucket, getBuckets } from "../../adapters/http.client.adapter.js";
+
+const CreateBucketBehavior = () => {
+  const setDisplayCreateBucket = useSetRecoilState(displayCreateBucketState);
+  const setDisplayCreateButton = useSetRecoilState(displayCreateButtonState);
+  const setTableResults = useSetRecoilState(bucketListTableResultsState);
+
   const [nameInputValue, setNameInputValue] = useState("");
   const [locationValue, setLocationValue] = useState("Kranj");
 
