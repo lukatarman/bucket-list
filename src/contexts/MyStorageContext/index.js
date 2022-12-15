832cc52643daf83bd9b791dce4f41dd10c485908
Luka Tarman <luka.tarman@gmail.com>
2022-12-15 14:27:22 +0100

    added recoil state context

---
+++ src/contexts/MyStorageContext/index.js
@@ -0,0 +1,21 @@
+import { atom } from "recoil";
+
+export const visibleTabState = atom({
+  key: "VisibleTab",
+  default: "file-list",
+});
+
+export const visibleDeleteState = atom({
+  key: "VisibleDelete",
+  default: false,
+});
+
+export const filesTableState = atom({
+  key: "VisiblePage",
+  default: [],
+});
+
+export const showDetailsAreltState = atom({
+  key: "ShowDetailsAlert",
+  default: false,
+});
