5c81ce437fb6e177bfa93da0075950d852862562
Luka Tarman <luka.tarman@gmail.com>
2022-12-16 12:33:43 +0100

    moved logic into file list behavior

---
+++ src/components/FileList/file.list.behavior.js
@@ -0,0 +1,51 @@
+import { useState, useEffect } from "react";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { uploadFile, getFiles, deleteFile } from "../../adapters/http.client.adapter.js";
+import { selectedBucketState } from "../../contexts/AppContext";
+import { filesTableState } from "../../contexts/MyStorageContext";
+
+const FileListBehavior = () => {
+  const selectedBucket = useRecoilValue(selectedBucketState);
+  const setFilesTable = useSetRecoilState(filesTableState);
+  const [showAlert, setShowAlert] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await getFiles(selectedBucket.index);
+      setFilesTable(response.data);
+    };
+
+    fetchData();
+  }, [setFilesTable]);
+
+  const handleFileUpload = async (e) => {
+    const formData = new FormData();
+    formData.append("file", e.target.files[0]);
+    await uploadFile(formData, selectedBucket.index);
+
+    const response = await getFiles(selectedBucket.index);
+
+    setFilesTable(response.data);
+  };
+
+  const handleDeleteButtonClick = async () => {
+    setShowAlert(true);
+  };
+
+  const handleDelete = async () => {
+    await deleteFile(selectedBucket.index);
+
+    const response = await getFiles(selectedBucket.index);
+
+    setFilesTable(response.data);
+  };
+  return [
+    showAlert,
+    setShowAlert,
+    handleDelete,
+    handleDeleteButtonClick,
+    handleFileUpload,
+  ];
+};
+
+export default FileListBehavior;
