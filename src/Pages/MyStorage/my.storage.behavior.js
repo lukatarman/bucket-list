0735c5a6094b5d6ea72b331b04fcfd077d9403c0
Luka Tarman <luka.tarman@gmail.com>
2022-12-16 12:23:25 +0100

    added content to my storage behavior file

--- src/Pages/MyStorage/my.storage.behavior.js
+++ src/Pages/MyStorage/my.storage.behavior.js
@@ -1,5 +1,34 @@
+import { useRecoilValue, useRecoilState, useSetRecoilState } from "recoil";
+import { useEffect } from "react";
+import { deleteBucket } from "../../adapters/http.client.adapter.js";
+import {
+  selectedBucketState,
+  visiblePageState,
+} from "../../contexts/AppContext/index.js";
+import {
+  visibleTabState,
+  visibleDeleteState,
+} from "../../contexts/MyStorageContext/index.js";
+
 const MyStorageBehavior = () => {
-  return [];
+  const selectedBucket = useRecoilValue(selectedBucketState);
+  const setVisiblePage = useSetRecoilState(visiblePageState);
+  const [visibleTab, setVisibleTab] = useRecoilState(visibleTabState);
+  const setVisibleDelete = useSetRecoilState(visibleDeleteState);
+
+  useEffect(() => {
+    visibleTab === "file-details" ? setVisibleDelete(true) : setVisibleDelete(false);
+  }, [visibleTab]);
+
+  const handleDeleteClick = () => {
+    deleteBucket(selectedBucket.index);
+    setVisiblePage("bucket-list");
+  };
+
+  const handleTabSelect = (tab) => {
+    setVisibleTab(tab);
+  };
+  return [handleDeleteClick, selectedBucket, visibleTab, handleTabSelect];
 };
 
 export default MyStorageBehavior;
