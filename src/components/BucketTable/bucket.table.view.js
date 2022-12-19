1790f556931cb385753a193758748baf1f5f8f05
Luka Tarman <luka.tarman@gmail.com>
2022-12-19 07:45:32 +0100

    refactored code

--- src/components/BucketTable/bucket.table.view.js
+++ src/components/BucketTable/bucket.table.view.js
@@ -1,33 +1,15 @@
-import { Table } from "react-bootstrap";
 import BucketTableBehavior from "./bucket.table.behavior.js";
-import BucketTableItem from "../BucketTableItem/bucket.table.item.js";
+
+import CustomTable from "../CustomTable/custom.table.view.js";
 
 const BucketTable = () => {
   const [tableResults, handleTableItemClick] = BucketTableBehavior();
 
-  const tableRender = tableResults.map((result, index) => {
-    return (
-      <BucketTableItem
-        key={index}
-        item={result}
-        index={index}
-        handleTableItemClick={handleTableItemClick}
-      />
-    );
-  });
-
   return (
-    <Table borderless hover>
-      <thead>
-        <tr className="table-row-bordered">
-          <th width="70%" className="table-head-content">
-            Name
-          </th>
-          <th className="table-head-content">Location</th>
-        </tr>
-      </thead>
-      {tableRender}
-    </Table>
+    <CustomTable
+      tableResults={tableResults}
+      handleTableItemClick={handleTableItemClick}
+    />
   );
 };
 
