e6b08010a115f37528a0ba105cfa962302f4453a
Luka Tarman <luka.tarman@gmail.com>
2023-01-12 17:12:46 +0100

    removed todo

--- src/Pages/App/app.view.js
+++ src/Pages/App/app.view.js
@@ -6,8 +6,6 @@ import MyStorage from "../MyStorage/my.storage.view.js";
 import { useRecoilState } from "recoil";
 import { visiblePageState } from "../../contexts/AppContext/index.js";
 
-//todo add using custom table to file.list.view.js
-
 const App = () => {
   const [visiblePage, setVisiblePage] = useRecoilState(visiblePageState);
 
