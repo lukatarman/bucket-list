e80c6cbb64ad02daf292e1f5f550ffcb9d993904
Luka Tarman <luka.tarman@gmail.com>
2023-01-13 16:11:44 +0100

    removed date.js file

--- backend/src/utils/date.js
+++
@@ -1,6 +0,0 @@
-export function fixDate() {
-  const currentDate = new Date();
-  const fixedDay = ("0" + currentDate.getDate()).slice(-2);
-  const fixedMonth = ("0" + (currentDate.getMonth() + 1)).slice(-2);
-  return `${fixedDay}.${fixedMonth}.${currentDate.getFullYear()}`;
-}
