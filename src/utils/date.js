3f95d5041b458667f75e7bd8acffc9d99e589564
Luka Tarman <luka.tarman@gmail.com>
2023-01-13 16:20:38 +0100

    fixed bug in function

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,6 +1,6 @@
-export function fixDate() {
-  const currentDate = new Date();
-  const fixedDay = ("0" + currentDate.getDate()).slice(-2);
-  const fixedMonth = ("0" + (currentDate.getMonth() + 1)).slice(-2);
-  return `${fixedDay}.${fixedMonth}.${currentDate.getFullYear()}`;
+export function fixDate(date) {
+  const fixedDate = new Date(date);
+  const fixedDay = ("0" + fixedDate.getDate()).slice(-2);
+  const fixedMonth = ("0" + (fixedDate.getMonth() + 1)).slice(-2);
+  return `${fixedDay}.${fixedMonth}.${fixedDate.getFullYear()}`;
 }
