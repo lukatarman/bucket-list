f4b48a851fcdee4c54ff484a95a06addf1ec05e8
Luka Tarman <luka.tarman@gmail.com>
2023-01-14 10:44:13 +0100

    added proper bucket size updating

--- src/components/FileList/file.list.behavior.js
+++ src/components/FileList/file.list.behavior.js
@@ -1,12 +1,17 @@
 import { useState, useEffect } from "react";
-import { useRecoilValue, useRecoilState } from "recoil";
-import { uploadFile, getFiles, deleteFile } from "../../adapters/http.client.adapter.js";
+import { useRecoilState } from "recoil";
+import {
+  getBuckets,
+  uploadFile,
+  getFiles,
+  deleteFile,
+} from "../../adapters/http.client.adapter.js";
 import { selectedBucketState } from "../../contexts/AppContext";
 import { filesTableState } from "../../contexts/MyStorageContext";
 import { fixFilesTable } from "./services/file.list.service.js";
 
 const FileListBehavior = (uploadButtonRef) => {
-  const selectedBucket = useRecoilValue(selectedBucketState);
+  const [selectedBucket, setSelectedBucket] = useRecoilState(selectedBucketState);
   const [filesTable, setFilesTable] = useRecoilState(filesTableState);
   const [showAlert, setShowAlert] = useState(false);
 
@@ -19,10 +24,6 @@ const FileListBehavior = (uploadButtonRef) => {
     fetchData();
   }, [setFilesTable]);
 
-  useEffect(() => {
-    console.log(filesTable);
-  }, [filesTable]);
-
   const handleFileUpload = async (e) => {
     const formData = new FormData();
     formData.append("file", e.target.files[0]);
@@ -30,6 +31,12 @@ const FileListBehavior = (uploadButtonRef) => {
 
     const response = await getFiles(selectedBucket.index);
 
+    const secondResponse = await getBuckets();
+
+    setSelectedBucket({
+      ...secondResponse.data[selectedBucket.index],
+      index: selectedBucket.index,
+    });
     setFilesTable(response.data);
   };
 
@@ -45,7 +52,12 @@ const FileListBehavior = (uploadButtonRef) => {
     await deleteFile(selectedBucket.index);
 
     const response = await getFiles(selectedBucket.index);
+    const secondResponse = await getBuckets();
 
+    setSelectedBucket({
+      ...secondResponse.data[selectedBucket.index],
+      index: selectedBucket.index,
+    });
     setFilesTable(response.data);
   };
 
