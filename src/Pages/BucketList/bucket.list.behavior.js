9a04ad608e90a7f441dc5f208dc7222c3ed4e706
Luka Tarman <luka.tarman@gmail.com>
2023-01-17 15:31:56 +0100

    added bucket list behavior component

---
+++ src/Pages/BucketList/bucket.list.behavior.js
@@ -0,0 +1,68 @@
+import { useEffect, useState } from "react";
+import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
+import { getBuckets } from "../../adapters/http.client.adapter.js";
+import { selectedBucketState, visiblePageState } from "../../contexts/AppContext";
+import {
+  bucketListTableResultsState,
+  displayCreateBucketState,
+  displayCreateButtonState,
+} from "../../contexts/BucketListContext";
+
+const BucketListBehavior = () => {
+  const [displayCreateBucket, setDisplayCreateBucket] = useRecoilState(
+    displayCreateBucketState
+  );
+  const [displayCreateButton, setDisplayCreateButton] = useRecoilState(
+    displayCreateButtonState
+  );
+  const tableResults = useRecoilValue(bucketListTableResultsState);
+  const [fetchResults, setFetchResults] = useRecoilState(bucketListTableResultsState);
+  const setSelectedBucket = useSetRecoilState(selectedBucketState);
+  const setVisiblePage = useSetRecoilState(visiblePageState);
+
+  const [tableValues, setTableValues] = useState({
+    head: ["Name", "Location"],
+    rows: [],
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await getBuckets();
+      setFetchResults(response.data);
+    };
+
+    fetchData();
+  }, [setFetchResults]);
+
+  useEffect(() => {
+    const tableResults = fetchResults.map((result) => [result.name, result.location]);
+
+    setTableValues({ head: tableValues.head, rows: tableResults });
+  }, [fetchResults]);
+
+  const handleButtonClick = () => {
+    setDisplayCreateButton(false);
+    setDisplayCreateBucket(true);
+  };
+
+  const handleTableItemClick = (index) => {
+    setSelectedBucket({
+      name: fetchResults[index].name,
+      location: fetchResults[index].location,
+      size: fetchResults[index].size,
+      index,
+    });
+    setVisiblePage("my-storage");
+  };
+
+  return [
+    displayCreateBucket,
+    displayCreateButton,
+    tableResults,
+    handleButtonClick,
+    handleTableItemClick,
+    tableValues,
+  ];
+};
+
+export default BucketListBehavior;
