52c8b365ab61c73b20bb4c58529f729ff5818c9f
Luka Tarman <luka.tarman@gmail.com>
2023-01-18 16:37:51 +0100

    added conditional removing border for last item

--- src/components/CustomTableItem/custom.table.item.js
+++ src/components/CustomTableItem/custom.table.item.js
@@ -1,10 +1,21 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFileLines } from "@fortawesome/free-solid-svg-icons";
 
-const CustomTableItem = ({ items, index, handleTableItemClick, variation }) => {
+const CustomTableItem = ({
+  items,
+  index,
+  handleTableItemClick,
+  variation,
+  lastIndex,
+}) => {
   const renderItems = items.map((item, i) => {
+    const lastBorder = index === lastIndex ? " border-bottom-0" : "";
+
     return (
-      <td key={i} className={variation && i === 0 ? "d-flex align-items-center" : ""}>
+      <td
+        key={i}
+        className={variation && i === 0 ? "d-flex align-items-center" + lastBorder : ""}
+      >
         {variation && i === 0 ? (
           <FontAwesomeIcon className="p-2" icon={faFileLines} size="xl" />
         ) : null}
