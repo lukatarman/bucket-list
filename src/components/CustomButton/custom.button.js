886e21ec7c218e1f09362be2cace184a006a8a33
Luka Tarman <luka.tarman@gmail.com>
2023-01-19 18:01:10 +0100

    added type argument to customButton component

--- src/components/CustomButton/custom.button.js
+++ src/components/CustomButton/custom.button.js
@@ -1,11 +1,11 @@
 import { Button } from "react-bootstrap";
 
-const CustomButton = ({ customClasses, handleClick, buttonValue }) => {
+const CustomButton = ({ customClasses, handleClick, buttonValue, type = "button" }) => {
   return (
     <Button
       className={`${customClasses} py-0`}
       variant="custom"
-      type="button"
+      type={type}
       onClick={handleClick}
     >
       {buttonValue}
