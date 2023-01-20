1e779a9321132ecfbc2ec4679665cdcc897bbba1
Luka Tarman <luka.tarman@gmail.com>
2023-01-20 11:02:36 +0100

    used bootstrap avaiable class for white text

--- src/Pages/App/app.view.js
+++ src/Pages/App/app.view.js
@@ -16,7 +16,7 @@ const App = () => {
 
   return (
     <div className="vh-100 bg-light">
-      <Navbar variant="custom" className="px-3 bg-secondary">
+      <Navbar className="px-3 bg-secondary text-white">
         <Container fluid>
           <h4 role="button" onClick={handleBrandClick}>
             Secure cloud storage
