e6f0e167cfe3a08e9e9c59163aea997824a1f7f2
Luka Tarman <luka.tarman@gmail.com>
2023-01-23 11:33:21 +0100

    optimized resettings filesTable on clicking brand

--- src/Pages/App/app.behavior.js
+++ src/Pages/App/app.behavior.js
@@ -4,16 +4,19 @@ import {
   displayCreateBucketState,
   displayCreateButtonState,
 } from "../../contexts/BucketListContext";
+import { filesTableState } from "../../contexts/MyStorageContext/index.js";
 
 const AppBehavior = () => {
   const [visiblePage, setVisiblePage] = useRecoilState(visiblePageState);
   const setDisplayCreateBucket = useSetRecoilState(displayCreateBucketState);
   const setDisplayCreateButton = useSetRecoilState(displayCreateButtonState);
+  const setFilesTable = useSetRecoilState(filesTableState);
 
   const handleBrandClick = () => {
     setVisiblePage("bucket-list");
     setDisplayCreateBucket(false);
     setDisplayCreateButton(true);
+    setFilesTable([]);
   };
 
   return [visiblePage, handleBrandClick];
