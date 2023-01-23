d4e60f12eee46fcf2a031380848bec26da38a295
Luka Tarman <luka.tarman@gmail.com>
2023-01-23 12:24:18 +0100

    commented out this.buffer property
    # added comment explaining situation

--- backend/src/models/file.js
+++ backend/src/models/file.js
@@ -4,11 +4,15 @@ export class File {
   size;
   buffer;
 
+  // The buffer property is commented out, as it causes large files to freeze the app.
+  // For larger files, a different method of saving data to the file should be used. It works with small files though.
+  // Comment it back in to enable it.
+
   //prettier-ignore
   constructor(fileDetails) {
     this.name         = fileDetails.originalname;
     this.lastModified = Date.parse(new Date());
     this.size         = fileDetails.size;
-    this.buffer       = fileDetails.buffer;
+    // this.buffer       = fileDetails.buffer;
   }
 }
