4c06d4d80d79bc6739be1cac0cd670ab41b0dc6d
Luka Tarman <luka.tarman@gmail.com>
2023-01-23 16:30:14 +0100

    added todo

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import ReactDOM from "react-dom/client";
 import App from "./pages/App/app.view.js";
 import "./assets/customStyles.css";
 
+//todo add proper size calculation in backend even when adding smaller files
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <RecoilRoot>
