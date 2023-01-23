ce1195b70b6be26c49c4fe02963861aca9011698
Luka Tarman <luka.tarman@gmail.com>
2023-01-23 20:19:45 +0100

    added todo

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import ReactDOM from "react-dom/client";
 import App from "./pages/App/app.view.js";
 import "./assets/customStyles.css";
 
-//todo add proper size calculation in backend even when adding smaller files
+// todo add proper size calculation in backend even when adding smaller files
+// fix rare backend bug that happens sometimes
+// https://i.imgur.com/hRHKRxp.png
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
