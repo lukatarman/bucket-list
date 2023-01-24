f25bf27946f3c4716bda4065c0e032d8abf292dc
Luka Tarman <luka.tarman@gmail.com>
2023-01-24 12:18:41 +0100

    removed todo

--- src/index.js
+++ src/index.js
@@ -4,11 +4,6 @@ import ReactDOM from "react-dom/client";
 import App from "./pages/App/app.view.js";
 import "./assets/customStyles.css";
 
-// todo add proper size calculation in backend even when adding smaller files
-// add file name in delete confirmation
-// fix rare backend bug that happens sometimes
-// https://i.imgur.com/hRHKRxp.png
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <RecoilRoot>
